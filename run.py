6a2df371f576f068eb3f5c17cb643073c3e4e648
cldelahan <cldelahan@gmail.com>
2018-10-07 13:46:42 -0400

    moved playing methods to play.py to simplify run.py

--- run.py
+++ run.py
@@ -1,77 +1,5 @@
 import game
-import sim
-
-def getRecomendedMovement(g1):
-    camels = ["Orange", "Yellow", "Green", "Blue", "White"]
-    sim1 = sim.Sim(g1)
-    # getting the maximum value from the simulations
-    shortTermEV = sim1.simShortTerm()
-    longTermEVW = sim1.simLongTerm()[0]
-    longTermEVL = sim1.simLongTerm()[1]
-    desertTileEV, tiles = sim1.simDesertTiles()
-    maxST = max(shortTermEV)
-    maxLTW = max(longTermEVW)
-    maxLTL = max(longTermEVL)
-    maxDT = max(desertTileEV)
-    # printing recomendations
-    if (maxST == max(maxST, maxLTW, maxLTL, maxDT, 1.0)):
-        index = shortTermEV.index(maxST)
-        print ("Bet Short Term")
-        print ("Camel: " + str(camels[index]) + " - " + str(index))
-        print ("Expected Value: " + str(maxST))
-    elif (maxDT == max(maxST, maxLTW, maxLTL, maxDT, 1.0)):
-        index = desertTileEV.index(maxDT)
-        print ("Place Desert Tile")
-        print ("Position: " + str(tiles[index]))
-        print ("Expected Value: " + str(maxST))
-    elif (maxLTW == max(maxST, maxLTW, maxLTL, maxDT, 1.0)):
-        index = longTermEVW.index(maxLTW)
-        print ("Bet Long Term Winner")
-        print ("Camel: " + str(camels[index]) + " - " + str(index))
-        print ("Expected Value: " + str(maxLTW))
-    elif (maxLTL == max(maxST, maxLTW, maxLTL, maxDT, 1.0)):
-        index = longTermEVL.index(maxLTL)
-        print ("Bet Long Term Loser")
-        print ("Camel: " + str(camels[index]) + " - " + str(index))
-        print ("Expected Value: " + str(maxLTL))
-    else:
-        print ("Roll")
-        print( "Expected Value: " + str(1.0))
-
-def playGame(g1):
-    # while there is no winner
-    while (g1.board.checkWinner() == -1):
-        print(g1)
-        print("Recommended Options:")
-        getRecomendedMovement(g1)
-        print("")
-        print("Movement Options")
-        print("1 - Roll\n2 - Bet Short-Term\n3 - Bet Long-Term\n4 - Place Backward DT\n5 - Place Forward DT")
-        movement = int(input("Selection: "))
-        if (movement == 1):
-            diceNum = int(input("Enter Color Number: "))
-            value = int(input("Enter Dice Value: "))
-            g1.roll(diceNum, value)
-            if (len(g1.diceRemaining) == 0):
-                g1.resetDiceRem()
-                g1.resetDesertTiles()
-                g1.resetPayout()
-        elif (movement == 2):
-            colorNumber = int(input("Enter Color Number: "))
-            g1.betShort(colorNumber)
-        elif (movement == 3):
-            winLose = int(input("Enter Win(0) or Lose (1): "))
-            g1.betLong(winLose)
-        elif (movement == 4):
-            pos = int(input("Enter position: "))
-            g1.placeBackwardTile(pos)
-        elif (movement == 5):
-            pos = int(input("Enter position: "))
-            g1.placeForwardTile(pos)
-        else:
-            print("Error: Operation invalid. Enter number 1-5")
-        print()
-        print()
+import play
         
 
 print("------------------------------")
@@ -89,4 +17,4 @@ org_tie, yel_tie, grn_tie, blu_tie, wht_tie = map(int, input("Enter initial tie 
 g1 = game.Game([org_pos, yel_pos, grn_pos, blu_pos, wht_pos], [org_tie, yel_tie, grn_tie, blu_tie, wht_tie])
 
 print()
-playGame(g1)
+play.playGame(g1)
